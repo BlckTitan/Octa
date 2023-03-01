153a6f1912029cb7d8416e52602d547f46ff005f
BlckTitan <dorshman406@gmail.com>
2023-03-01 13:12:50 +0100

    Created the Navbar styling'

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,6 +14,20 @@ export default function NavBar() {
             </Typography.Title>
             {/*<button className='menu-control-container'></button>*/}
         </div>
+        <Menu theme='dark'>
+            <Menu.Item icon={<HomeOutlined />}>
+                <Link to="/">Home</Link>
+            </Menu.Item>
+            <Menu.Item icon={<FundOutlined />}>
+                <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+            </Menu.Item>
+            <Menu.Item icon={<MoneyCollectOutlined />}>
+                <Link to="/exchanges">Exchanges</Link>
+            </Menu.Item>
+            <Menu.Item icon={<BulbOutlined />}>
+                <Link to="/news">News</Link>
+            </Menu.Item>
+        </Menu>
     </div>
   )
 }
