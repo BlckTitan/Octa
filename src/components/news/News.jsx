a7139dfb502ece9e829d3c09204a67878eeacc99
BlckTitan <dorshman406@gmail.com>
2023-03-10 15:28:56 +0100

    Completed the news card, queried the news source and published info

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useGetCryptoNewsQuery } from '../../app/newsSlice';
-import { Grid } from '@mui/material';
-import { Card, Typography, CardMedia, CardContent} from '@mui/material';
+import { Avatar, Grid } from '@mui/material';
+import { Card, Typography, CardContent} from '@mui/material';
 import './style/news_style.scss';
+import moment from 'moment/moment';
 
 
 
@@ -21,23 +22,43 @@ export default function News(simplified) {
         {cryptoNews.value && cryptoNews.value.map((cryproNewsData, index) => (
           <Grid item key={index} xs={3}>
             <Link to={cryproNewsData.url} target='_blank'>
-              <Card sx={{height: 350}} className='card'>
+              <Card sx={{height: 230}} className='card'>
+                <header>
 
-                <img 
-                  src={cryproNewsData?.image?.thumbnail?.contentUrl}
-                 alt='Crypto news thumbnail'
-                />
+                  <img 
+                    src={cryproNewsData?.image?.thumbnail?.contentUrl}
+                  alt='Crypto news thumbnail'
+                  />
+                  
+                  <Typography gutterBottom variant="h6" component="div" className='news_title'>
+                      {cryproNewsData.name}
+                  </Typography>
+
+                </header>
 
                 <CardContent>
 
-                  <Typography gutterBottom variant="h6" component="div">
-                    {cryproNewsData.name}
-                  </Typography>
-
                   <Typography variant="body2" color="text.secondary">
-                    {cryproNewsData.description.substring(0, 80)}
+                    {cryproNewsData.description.substring(0, 100)}
                   </Typography>
+                  
+                  <div className='newsSource'>
+                    <div className='provider'>
 
+                      <Avatar 
+                        src={cryproNewsData?.provider[0]?.image?.thumbnail?.contentUrl} 
+                        alt=''
+                      />
+
+                      <span>{cryproNewsData?.provider[0]?.name}</span>
+
+                    </div>
+
+                    <span>
+                      {moment(cryproNewsData.datePublished).startOf('seconds').fromNow()}
+                    </span>
+
+                  </div>
                 </CardContent>
               </Card>
             </Link>
