a6cc87c5523512c16f27d0f56964ad9aa0bb059a
BlckTitan <dorshman406@gmail.com>
2023-03-16 19:55:36 +0100

    Completed the exchanges page

--- src/components/exchanges/Exchanges.jsx
+++ src/components/exchanges/Exchanges.jsx
@@ -3,21 +3,20 @@ import React, { useState } from 'react';
 import { useGetCoinExchangesByIdQuery, useGetCryptoCoinsQuery } from '../../app/coinSlice';
 //style
 import './style/exchange_style.scss';
-//icon
-import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
+//material
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { Avatar } from '@mui/material';
+//icon
+import VerifiedIcon from '@mui/icons-material/Verified';
+import VerifiedOutlinedIcon from '@mui/icons-material/VerifiedOutlined';
+//millify
+import millify from 'millify';
 
 export default function Exchanges({simplified}) {
 
@@ -27,39 +26,61 @@ export default function Exchanges({simplified}) {
   const {data: coinExchange, isFetching} = useGetCoinExchangesByIdQuery({coinId, count})
   const {data: allCoins} = useGetCryptoCoinsQuery(count)
   let coinDetailsInfo = allCoins?.data?.coins
-
-  console.log(coinExchange)
+  let coinExchangeInfo = coinExchange?.data?.exchanges;
 
   if(isFetching) return 'Loading';
   
   return (
     <div className='exchange_container'>
       <header>
-        <select onChange={(e) => setCoinId(e.target.value)}>
-          {coinDetailsInfo && coinDetailsInfo.map((coinDetail, index) => (
+          <select onClick={(e) => {setCoinId(e.target.value)}}>
+            {coinDetailsInfo && coinDetailsInfo.map((coinDetail, index) => (
 
-            <option key={index} value={coinDetail.uuid}>{coinDetail.name}</option>
-            
-          ))}
+              <option key={index} value={coinDetail.uuid}>{coinDetail.name}</option>
+              
+            ))}
           </select>
       </header>
       
       <div className='exchange_body'>
         <TableContainer component={Paper}>
           <Table aria-label="collapsible table">
+
             <TableHead>
               <TableRow>
-                <TableCell>Dessert (100g serving)</TableCell>
-                <TableCell align="left">Calories</TableCell>
-                <TableCell align="left">Fat&nbsp;(g)</TableCell>
-                <TableCell align="left">Carbs&nbsp;(g)</TableCell>
-                <TableCell align="left">Protein&nbsp;(g)</TableCell>
+                <TableCell sx={{fontWeight: '600', color: '#232323'}} align="left">Exchanges</TableCell>
+                <TableCell sx={{fontWeight: '600', color: '#232323'}} align="left">Rank</TableCell>
+                <TableCell sx={{fontWeight: '600', color: '#232323'}} align="left">24H Trade Volume</TableCell>
+                <TableCell sx={{fontWeight: '600', color: '#232323'}} align="left">Price</TableCell>
+                <TableCell sx={{fontWeight: '600', color: '#232323', display: 'flex', alignItems: 'center'}} align="left">
+                  <VerifiedIcon sx={{color: '#232323', marginRight: '1rem'}}/>
+                  <span>Verified</span>
+                </TableCell>
               </TableRow>
             </TableHead>
+
             <TableBody>
+
+              
+              {coinExchangeInfo && coinExchangeInfo.map((exchange, index) => (
+                <TableRow key={index}>
+                  <TableCell align="left" sx={{display: 'flex', alignItems: 'center'}}>
+                    <Avatar src={exchange.iconUrl} sx={{marginRight: '1rem'}}/>
+                    <span>{exchange.name}</span>
+                  </TableCell>
+                  <TableCell align="left">{exchange.rank}</TableCell>
+                  <TableCell align="left">{millify(exchange?.['24hVolume'])}</TableCell>
+                  <TableCell align="left">{millify(exchange.price)}</TableCell>
+                  <TableCell align="left">
+                    {exchange.verified === true ? <VerifiedIcon sx={{color: '#2979ff'}}/> : <VerifiedOutlinedIcon/>}
+                  </TableCell>
+                </TableRow>
+              ))}
+
               
             </TableBody>
           </Table>
+
         </TableContainer>
       </div>
     </div>
